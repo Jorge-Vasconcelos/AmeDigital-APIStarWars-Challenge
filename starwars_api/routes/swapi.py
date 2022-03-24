30fdf25f61b14b67cd86d31134d778c2dabe2e87
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-24 09:40:22 -0300

    feat: name and qtd in route /swapi

--- starwars_api/routes/swapi.py
+++ starwars_api/routes/swapi.py
@@ -1,9 +1,20 @@
-import requests
+from flask import jsonify, make_response
 from flask_restful import Resource
+import requests
 
 
 class Swapi(Resource):
     def get(self):
-        r = requests.get('https://swapi.dev/api/planets/')
-        data = r.json()
-        return data, 200
+        planet_list = []
+        next_request = 'https://swapi.dev/api/planets/'
+        while next_request:
+            response = requests.get(next_request).json()
+            next_request = response['next']
+            for planet in response['results']:
+                planet_dict = {
+                    'name': planet['name'],
+                    'qtd_films': len(planet['films'])
+                }
+                planet_list.append(planet_dict)
+
+        return make_response(jsonify(planet_list), 200)
